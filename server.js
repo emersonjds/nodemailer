e10d362d253aa5a926a91e944d124876576eba5c
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-21 15:52:50 -0300

    add a git ignore

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var port = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let transport = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'xxxxx', //email user
+        pass: 'xxxxxx' // password user
+    }
+})
+
+app.post('/sendEmail', (req, res) => {
+
+    let email = {
+        from: req.body.email,
+        to: 'xxxxx', //destiny email
+        subject: 'Assunto',
+        html: req.body.message
+    }
+
+    transport.sendMail(email, (err, info) => {
+        if (err) {
+            throw console.log(err.message);
+        }
+        console.log('email enviado')
+    })
+
+    res.send(req.body);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port} !`)
+})
